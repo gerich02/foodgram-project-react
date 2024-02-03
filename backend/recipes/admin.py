574a439fa42583b2a297776ee2a093c3ac40f30a
unknown <ger-adv@yandex.ru>
2024-02-03 23:11:42 +0300

    Simple Tag model added

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from recipes.models import Ingredient
+from recipes.models import Ingredient, Tag
 
 admin.site.register(Ingredient)
+admin.site.register(Tag)
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название')
+    color = models.CharField(max_length=200, verbose_name='Цвет в HEX')
+    slug = models.SlugField(max_length=200, verbose_name='Уникальный слаг')
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=200, verbose_name='Название')
     measurement_unit = models.CharField(
